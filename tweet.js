f90a5fa1086b03b775e1db9d85dc5b141c3f3bd8
siranovel <siranovel@gmail.com>
2024-02-19 12:39:31 +0900

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -5,7 +5,7 @@ const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env,TWITTER_CCESS_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_CCESS_TOKEN_SECRET
 });
 
 const tweet = 'New commit pushed to ${process.env.GITHUB_REPOSITORY}!';
