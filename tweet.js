b117bd72ed244984b9367abb48b59cf3c0704aaf
siranovel <siranovel@gmail.com>
2024-02-19 17:16:36 +0900

    tweet.js の更新

--- tweet.js
+++ tweet.js
@@ -3,7 +3,7 @@ const Twitter = require('twitter-lite');
 const client = new Twitter({
   subdomain: "api",
   version: ”2”,
-  extension: false
+  extension: false,
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
