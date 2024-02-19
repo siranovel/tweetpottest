95c44e130770266df5347210481dcdc68239a81a
siranovel <siranovel@gmail.com>
2024-02-19 12:28:11 +0900

    Add files via upload

---
+++ tweet.js
@@ -0,0 +1,22 @@
+const Titter = require('twitter-lite');
+
+const client = new Twitter({
+  subdoain: "api",
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env,TWITTER_CCESS_TOKEN_SECRET
+});
+
+const tweet = 'New commit pushed to ${process.env.GITHUB_REPOSITORY}!';
+
+client
+  .post("statuses/update", {
+    status: tweet,
+  })
+  .then((result) => {
+    console.log("Tweeted successfully!");
+  })
+  .catch((error) =>{
+    console.error(error);
+  });
