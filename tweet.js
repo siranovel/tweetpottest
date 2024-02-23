7618496f8bade2c00c4de41f99c411adf7078ff0
siranovel <siranovel@gmail.com>
2024-02-23 11:25:06 +0900

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -24,7 +24,6 @@ const client = new Twitter({
   subdomain: "api",
   version: "2",
   extension: false,
-  bearer_token: response.access_token,
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
