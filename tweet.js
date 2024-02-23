fab9e1737a95c45dfb43eaa540be2189a2188eff
siranovel <siranovel@gmail.com>
2024-02-23 11:23:47 +0900

    Add files via upload

--- tweet.js
+++ tweet.js
@@ -1,22 +1,37 @@
 const Twitter = require('twitter-lite');
-
 const user = new Twitter({
+  subdomain: "api",
+  version: "2",
+  extension: false,
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 });
-// const response = getBearerToken();
+user
+  .post("oauth/request_token")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+
+
+
+// const response = await user.getBearerToken();
 const client = new Twitter({
   subdomain: "api",
-  version: "1.1",
+  version: "2",
   extension: false,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+  bearer_token: response.access_token,
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_CCESS_TOKEN_SECRET,
 });
 
-const tweet = 'New commit pushed ';
+const tweet = 'New commit pushed to ${process.env.GITHUB_REPOSITORY}!';
 
 client
   .post("statuses/update", {
