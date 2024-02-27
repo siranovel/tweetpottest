2b6f904bcce1d59dea17cb5b44dcf7056fc6873f
siranovel <siranovel@gmail.com>
2024-02-27 13:13:44 +0900

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -7,7 +7,7 @@ const user = new Twitter({
 });
 user
   .get("oauth/token", {
-    response_type=code,
+    response_type='code',
     redirect_uri='https://twitter.com/',
     scope='tweet.write',
   })
