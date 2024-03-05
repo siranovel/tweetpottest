92c5340782769ade6906f9c8e649665acb6a32ba
siranovel <siranovel@gmail.com>
2024-03-05 20:23:27 +0900

    tweet.js の更新

--- tweet.js
+++ tweet.js
@@ -29,7 +29,7 @@ const app = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_CCESS_TOKEN_SECRET,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const tweet = 'New commit pushed to ${process.env.GITHUB_REPOSITORY}!';
