860e050a1c978de1fc2c36883c84aa3cecda8f01
siranovel <siranovel@gmail.com>
2024-03-13 23:13:51 +0900

    tweetx.py の更新

--- tweetx.py
+++ tweetx.py
@@ -9,7 +9,7 @@ ACCESS_KEY_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 # オブジェクト作成
 client = tweepy.Client(
     consumer_key = CONSUMER_KEY,
-    consumer_key_secret = CONSUMER_SECRET,
+    consumer_secret = CONSUMER_SECRET,
     access_token = ACCESS_KEY,
     access_token_secret = ACCESS_KEY_SECRET
 )
