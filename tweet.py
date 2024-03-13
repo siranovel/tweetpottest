40a4cbc61569c9a6ba30f5d4584c161c7539f3c6
siranovel <siranovel@gmail.com>
2024-03-13 12:27:21 +0900

    Add files via upload

--- tweet.py
+++ tweet.py
@@ -12,18 +12,20 @@ base_authorization_url = 'https://api.twitter.com/oauth/authorize'
 access_token_url = 'https://api.twitter.com/oauth/access_token'
 url_text = 'https://api.twitter.com/1.1/statuses/update.json'
 tweet = 'New commit pushed!'
-scope = "tweet.write,tweet.read"
 def main():
     # OAuth1Sessionの認証処理
     oauth = OAuth1Session(CONSUMER_KEY, client_secret=CONSUMER_SECRET, callback_uri='https://twitter.com/')
     fetch_response = oauth.fetch_request_token(request_token_url)
     resource_owner_key = fetch_response.get('oauth_token')
     resource_owner_secret = fetch_response.get('oauth_token_secret')
-    authorization_url = oauth.authorization_url(base_authorization_url)
-    print( authorization_url)
-    ridirect_response = authorization_url+"&response_type=code&scope="+scope
-    oauth_response =  oauth.parse_authorization_response(ridirect_response)
-    print("ddddddddddddddddddd")
+    authorization_url = oauth.authorization_url(
+        base_authorization_url, 
+        response_type=code,
+        scope='tweet.write,tweet.read')
+    print(authorization_url)
+    ridirect_response = oauth.parse_authorization_response(authorization_url)
+    print(ridirect_response)
+    oauth_response = oauth.get(base_authorization_url, params=ridirect_response)
     print(oauth_response)
     print("aaaaaaaaaaaaaaaaaaaaa")
     res = oauth.post(url_text, params = {'status': tweet})
