ace4f999b91110f798f9b5ae67ab19a3741d5114
siranovel <siranovel@gmail.com>
2024-03-18 11:04:59 +0900

    tweet2.py の更新

--- tweet2.py
+++ tweet2.py
@@ -24,7 +24,6 @@ def main():
     oauth = OAuth2Session(CLIENT_ID)
     authorization_url = oauth.authorization_url(
         base_authorization_url, 
-        response_type='code',
         client_id = CLIENT_ID,
         redirect_url = 'https://twitter.com/',
         state='state',
