f136b8800e3ebdb9831b5bca1e0d0a2108ecdac5
siranovel <siranovel@gmail.com>
2024-03-21 10:12:02 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -28,7 +28,7 @@ def main():
                             "grant_type": "client_credentials"
                         })
     print(oauth_response)
-    print(oauth_response.text)
+    print(oauth_response.json())
     print(type(oauth_response))
     res = requests.post(url_text, params = {'text': tweet})
     print(res)
