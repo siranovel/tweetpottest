ac18d4434a561c52f7e730684f8ab2f078961078
siranovel <siranovel@gmail.com>
2024-03-29 09:50:34 +0900

    Add files via upload

--- tweet.py
+++ tweet.py
@@ -1,61 +1,56 @@
-from requests.auth     import HTTPBasicAuth
-from requests_oauthlib import OAuth1Session
-import requests
 import os
-import base64
+import urllib.parse as parse
+import requests
 
 CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
 ACCESS_KEY = os.environ.get("TWITTER_ACCESS_TOKEN")
 ACCESS_KEY_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-
-request_token_url = 'https://api.twitter.com/oauth/request_token'
-base_authorization_url = 'https://api.twitter.com/oauth/authorize'
-token_url = 'https://api.twitter.com/oauth2/token'
-url_text = 'https://api.twitter.com/2/tweets'
+SIGNATURE_METHOD = 'PLAIN'
 tweet = 'New commit pushed! (twitter v2 oauth 1.0a)'
+url_text = 'https://api.twitter.com/2/tweets'
 
-def createHeaders(consumer_key, consumer_secret, access_token, access_token_secret):
-    baseparam = {
-        "oauth_token": access_token,
-        "oauth_consumer_key": consumer_key,
-        "oauth_signature_metod": "MAC-SHA1",
-        "oauth_timestamp": str(int(time.time())),
-        "oauth_nonce": str(random.getrandbits(64)),
-        "oauth_version": "1.0"
-    }
-    signature = dict(baseparam)
+def _conv_dict(dict_: dict):
+    string = ''
+    for key, val in dict_.items():
+        string += key + '="' + val + '", '
+    return string.rstrip(', ')
+
+def _create_signature():
+    clent_id64 = CONSUMER_KEY + ":" + CONSUMER_SECRET
+    token_id64 = ACCESS_KEY + ":" + ACCESS_KEY_SECRET
+    base_string = clent_id64
+                + '%30'
+                + token_id64
+    return base64.b64encode(base_string.encode()).decode()
+
+def _create_authorization_params():
+    auth_params = {}
+    auth_params['oauth_consumer_key'] = parse.quote(CONSUMER_KEY, safe='')
+    auth_params['oauth_token'] = parse.quote(ACCESS_KEY, safe='')
+    auth_params['oauth_signature_method'] = SIGNATURE_METHOD
+    auth_params['oauth_version'] = '1.0'
+    auth_params['oauth_signature'] = _create_signature()
+
+    return auth_params
+
+def _create_authorization_header():
+    auth_headr_val = 'OAuth '
+    params = _create_authorization_params()
+    auth_headr_val += _conv_dict(params)
+
+    return {'Authorization': auth_headr_val}
 
 def main():
-    # OAuth1
-    # step 1
-    req_response = requests.post(request_token_url, 
-                     params={
-                         "oauth_callback": "oob",
-                         "x_auth_access_type": "write"
-                     })
-    print(req_response)
-    print(req_response.text)
-    # step 2
-    text = CONSUMER_KEY + ":" + CONSUMER_SECRET
-    token_headers={
-        'Authorization': 'Basic ' + base64.b64encode(text.encode()).decode()
-    }
-    token_response = requests.post(token_url, 
-                        headers = token_headers,
-                        params={
-                            "grant_type": "client_credentials"
-                        }).json()
-    print(token_response['access_token'])
-    tweet_headers={
-        'Authorization': 'Bearer ' + token_response['access_token']
-    }
-    res = requests.post(url_text, 
-                        headers = tweet_headers,
-                        params = {'text': tweet})
+    ctx_headers = _create_authorization_header()
+    res = requests.post(url_text,
+                        headers = ctx_headers,
+                        params = {'text': tweet}
+    )
     print(res)
     print(res.text)
-
+                        
+    
 if __name__ == "__main__":
     main()
 
