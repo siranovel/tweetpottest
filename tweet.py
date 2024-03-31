117a0b7554fea54dd88caff7a242225207342912
siranovel <siranovel@gmail.com>
2024-03-31 10:07:25 +0900

    Add files via upload

--- tweet.py
+++ tweet.py
@@ -1,56 +1,41 @@
 from requests_oauthlib import OAuth1Session
-
 import os
-import urllib.parse as parse
 import base64
-import requests
 
 CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
 ACCESS_KEY = os.environ.get("TWITTER_ACCESS_TOKEN")
 ACCESS_KEY_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-SIGNATURE_METHOD = 'PLAIN'
 tweet = 'New commit pushed! (twitter v2 oauth 1.0a)'
 request_token_url = 'https://api.twitter.com/oauth/request_token'
+token_url = 'https://api.twitter.com/oauth2/token'
 url_text = 'https://api.twitter.com/2/tweets'
 
-def _conv_dict(dict_: dict):
-    string = ''
-    for key, val in dict_.items():
-        string += key + '="' + val + '", '
-    return string.rstrip(', ')
-
-def _create_signature():
-    clent_id64 = CONSUMER_KEY + ":" + CONSUMER_SECRET
-    token_id64 = ACCESS_KEY + ":" + ACCESS_KEY_SECRET
-    base_string = clent_id64 + chr(38) + token_id64
-    return base64.b64encode(base_string.encode()).decode()
-
-def _create_authorization_params():
-    auth_params = {}
-    auth_params['oauth_consumer_key'] = parse.quote(CONSUMER_KEY, safe='')
-    auth_params['oauth_token'] = parse.quote(ACCESS_KEY, safe='')
-    auth_params['oauth_signature_method'] = SIGNATURE_METHOD
-    auth_params['oauth_version'] = '1.0'
-    auth_params['oauth_signature'] = _create_signature()
-
-    return auth_params
-
-def _create_authorization_header():
-    auth_headr_val = 'OAuth '
-    params = _create_authorization_params()
-    auth_headr_val += _conv_dict(params)
-
-    return {'Authorization': auth_headr_val}
 
 def main():
-    oauth = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY)
-    token_res = oauth.post(request_token_url, params={'oauth_callback': 'oob'})
-    print(token_res.text)
-    ctx_headers = _create_authorization_header()
-    print(ctx_headers)
-    res = requests.post(request_token_url,
-                        headers = ctx_headers)
+    twitter = OAuth1Session(CONSUMER_KEY, 
+                            client_secret = CONSUMER_SECRET,
+                            resource_owner_key=ACCESS_KEY,
+                            resource_owner_secret=ACCESS_KEY_SECRET)
+    #
+    fetch_response = twitter.fetch_request_token(request_token_url,
+                            params={"x_auth_access_type": 'write'})
+    print(token_response)
+    resource_owner_key = fetch_response.get('oauth_token')
+    resource_owner_secret = fetch_response.get('oauth_token_secret')
+    #
+    text = CONSUMER_KEY + ":" + CONSUMER_SECRET
+    token_headers={
+        'Authorization': 'Basic ' + base64.b64encode(text.encode()).decode()
+    }
+    oauth_response = twitter.post(token_url, 
+                        headers = token_headers,
+                        params={
+                            "grant_type": "client_credentials"
+                        })
+    print(oauth_response)
+    #
+    res = twitter.post(url_text, params={"text": url_text})
     print(res)
     print(res.text)
                         
