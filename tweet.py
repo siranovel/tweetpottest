43ea67f3b83bdae411089c8905dfbc9c201dbf24
siranovel <siranovel@gmail.com>
2024-04-01 10:05:35 +0900

    Add files via upload

--- tweet.py
+++ tweet.py
@@ -9,35 +9,39 @@ ACCESS_KEY = os.environ.get("TWITTER_ACCESS_TOKEN")
 ACCESS_KEY_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 tweet = 'New commit pushed! (twitter v2 oauth 1.0a)'
 request_token_url = 'https://api.twitter.com/oauth/request_token'
-auth_url = 'https://api.twitter.com/i/oauth2/token'
 token_url = 'https://api.twitter.com/oauth2/token'
+access_token_url = 'https://api.twitter.com/oauth/access_token'
 url_text = 'https://api.twitter.com/2/tweets'
 
-
 def main():
-    ses = requests.Session()
-    twitter = OAuth1Session(CONSUMER_KEY, 
-                            client_secret = CONSUMER_SECRET,
-                            resource_owner_key=ACCESS_KEY,
-                            resource_owner_secret=ACCESS_KEY_SECRET)
-    #
-    fetch_response = twitter.fetch_request_token(request_token_url,
-                            params={"x_auth_access_type": 'write'})
-    print(fetch_response)
-    resource_owner_key = fetch_response.get('oauth_token')
-    resource_owner_secret = fetch_response.get('oauth_token_secret')
     #
     text = CONSUMER_KEY + ":" + CONSUMER_SECRET
     token_headers={
         'Authorization': 'Basic ' + base64.b64encode(text.encode()).decode()
     }
-    oauth_response = twitter.post(token_url, 
+    oauth_response = requests.post(token_url, 
+                        headers = token_headers,
                         params={
-                            'Authorization': 'Basic ' + base64.b64encode(text.encode()).decode(),
                             "grant_type": "client_credentials"
                         }).json()
     print(oauth_response)
     print(oauth_response['access_token'])
+    verifier = oauth_response['access_token']
+    #
+    twitter = OAuth1Session(CONSUMER_KEY, 
+                            client_secret = CONSUMER_SECRET,
+                            resource_owner_key=ACCESS_KEY,
+                            resource_owner_secret=ACCESS_KEY_SECRET,
+                            verifier=verifier)
+    #
+    fetch_response = twitter.fetch_request_token(request_token_url,
+                            params={"x_auth_access_type": 'write'})
+    print(token_response)
+    resource_owner_key = fetch_response.get('oauth_token')
+    resource_owner_secret = fetch_response.get('oauth_token_secret')
+    #
+    token_response = twitter.fetch_access_token(access_token_url)
+    print(token_response)
     #
     res = twitter.post(url_text, params={"text": url_text})
     print(res)
