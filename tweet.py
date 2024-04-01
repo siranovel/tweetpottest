cae450d650d0e9eabaaa7258c19d9946178aaaf7
siranovel <siranovel@gmail.com>
2024-04-01 10:28:36 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -15,6 +15,18 @@ url_text = 'https://api.twitter.com/2/tweets'
 
 def main():
     #
+    twitter = OAuth1Session(CONSUMER_KEY, 
+                            client_secret = CONSUMER_SECRET,
+                            resource_owner_key=ACCESS_KEY,
+                            resource_owner_secret=ACCESS_KEY_SECRET
+                            )
+    #
+    fetch_response = twitter.fetch_request_token(request_token_url,
+                            params={"x_auth_access_type": 'write'})
+    print(fetch_response)
+    resource_owner_key = fetch_response.get('oauth_token')
+    resource_owner_secret = fetch_response.get('oauth_token_secret')
+    #
     text = CONSUMER_KEY + ":" + CONSUMER_SECRET
     token_headers={
         'Authorization': 'Basic ' + base64.b64encode(text.encode()).decode()
@@ -31,19 +43,7 @@ def main():
     print(oauth_response['access_token'])
     verifier = oauth_response['access_token']
     #
-    twitter = OAuth1Session(CONSUMER_KEY, 
-                            client_secret = CONSUMER_SECRET,
-                            resource_owner_key=ACCESS_KEY,
-                            resource_owner_secret=ACCESS_KEY_SECRET
-                            )
-    #
-    fetch_response = twitter.fetch_request_token(request_token_url,
-                            params={"x_auth_access_type": 'write'})
-    print(fetch_response)
-    resource_owner_key = fetch_response.get('oauth_token')
-    resource_owner_secret = fetch_response.get('oauth_token_secret')
-    #
-    token_response = twitter.fetch_access_token(access_token_url, verifier=resource_owner_key)
+    token_response = twitter.fetch_access_token(access_token_url, verifier=verifier)
     print(token_response)
     #
     res = twitter.post(url_text, params={"text": url_text})
