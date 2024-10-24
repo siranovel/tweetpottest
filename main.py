77b8d8a0fb6558cbdfe6373f53c40dc92ec29d78
siranovel <24727362+siranovel@users.noreply.github.com>
2024-10-24 11:50:00 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+def main() -> None:
+    repository = os.getenv("INPUT_REPOSITORY")
+    print(f"Hello world from github actions in {repository}!")
+
+if __name__ == "__main__":
+    main()
