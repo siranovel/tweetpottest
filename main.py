1fe99ce0e71fd45065f619b9705b4ed3978095d4
siranovel <24727362+siranovel@users.noreply.github.com>
2024-10-24 14:45:00 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 def main() -> None:
-    repository = os.getenv("INPUT_REPOSITORY")
+    repository = os.getenv("INPUT_TEXT")
     print(f"Hello world from github actions in {repository}!")
 
 if __name__ == "__main__":
