d47816f75d0bfd6000e9872f30bbb484bcff8781
siranovel <24727362+siranovel@users.noreply.github.com>
2024-10-25 11:46:50 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import os
+import tweepy
 
 def main() -> None:
-    repository = os.getenv("INPUT_TEXT")
-    print(f"Hello world from github actions in {repository}!")
+    tweet = os.getenv("INPUT_TEXT")
+    CONSUMER_KEY = os.environ.get("INPUT_TWITTER_CONSUMER_KEY")
+    CONSUMER_SECRET = os.environ.get("INPUT_TWITTER_CONSUMER_SECRET")
+    ACCESS_KEY = os.environ.get("INPUT_TWITTER_ACCESS_TOKEN")
+    ACCESS_KEY_SECRET = os.environ.get("INPUT_TWITTER_ACCESS_TOKEN_SECRET")
+
+    # オブジェクト作成
+    client = tweepy.Client(
+        consumer_key = CONSUMER_KEY,
+        consumer_secret = CONSUMER_SECRET,
+        access_token = ACCESS_KEY,
+        access_token_secret = ACCESS_KEY_SECRET)
+    # ツィートする
+    res = client.create_tweet(text=tweet)
+    print(res)
 
 if __name__ == "__main__":
     main()
